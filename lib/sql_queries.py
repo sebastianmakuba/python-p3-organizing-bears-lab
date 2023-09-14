3b9a5f4c4195f2c4704aebe20a7e0a2f81ce45b0
+ <+>
2023-09-15 00:08:02 +0300

    completed assignment

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,37 @@
+# sql_queries.py
+
+# CREATE TABLE statement for the "bears" table
+create_bears_table = """
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER
+);
+"""
+
+# Other SQL queries as previously provided
+# ...
+
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT name, age FROM bears WHERE sex = 'female';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT name FROM bears ORDER BY name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;
 """
+
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;
 """
